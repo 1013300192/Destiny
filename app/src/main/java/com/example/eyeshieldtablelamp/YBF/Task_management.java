991db629c453786803ef5f9286664f19ac24da10
Destiny <1013300192@qq.com>
2021-08-10 22:42:02 +0800

    Ybf 8.10

--- app/src/main/java/com/example/eyeshieldtablelamp/YBF/Task_management.java
+++ app/src/main/java/com/example/eyeshieldtablelamp/YBF/Task_management.java
@@ -1,8 +1,12 @@
 package com.example.eyeshieldtablelamp.YBF;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.eyeshieldtablelamp.R;
 
@@ -12,5 +16,26 @@ public class Task_management extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_management);
+    }
+
+    /**
+     * 重写方法，创建menu
+     **/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    /**
+     * 重写方法，为menu设置点击事件
+     **/
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_task){
+            //调用数据库添加操作
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
