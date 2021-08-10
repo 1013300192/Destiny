3820fe9a34ed372af74bb5b7d53dec29fe01109f
Destiny <1013300192@qq.com>
2021-08-10 21:35:44 +0800

    Ybf

---
+++ app/src/main/java/com/example/eyeshieldtablelamp/YBF/Personal_information.java
@@ -0,0 +1,16 @@
+package com.example.eyeshieldtablelamp.YBF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eyeshieldtablelamp.R;
+
+public class Personal_information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_information);
+    }
+}
---
+++ app/src/main/java/com/example/eyeshieldtablelamp/YBF/Task_management.java
@@ -0,0 +1,16 @@
+package com.example.eyeshieldtablelamp.YBF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eyeshieldtablelamp.R;
+
+public class Task_management extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_management);
+    }
+}
