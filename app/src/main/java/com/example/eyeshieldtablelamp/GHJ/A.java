b158b8186abe3a1f9f155d3e389e542793452e27
Destiny <1013300192@qq.com>
2021-08-10 21:38:58 +0800

    Ybf

---
+++ app/src/main/java/com/example/eyeshieldtablelamp/GHJ/A.java
@@ -0,0 +1,16 @@
+package com.example.eyeshieldtablelamp.GHJ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eyeshieldtablelamp.R;
+
+public class A extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+    }
+}
---
+++ app/src/main/java/com/example/eyeshieldtablelamp/LXY/B.java
@@ -0,0 +1,16 @@
+package com.example.eyeshieldtablelamp.LXY;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.eyeshieldtablelamp.R;
+
+public class B extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
